e7c0d1c68b5bd0bdc649258d8bc5b8f93d72f696
its-marten <63053532+its-marten@users.noreply.github.com>
2022-02-15 16:13:09 +0100

    Update cryptostreamlit.py

--- cryptostreamlit.py
+++ cryptostreamlit.py
@@ -19,32 +19,6 @@ st.header("Step 1: Collecting Data")
 st.write("In every step I will show the code in python if you would be interested in knowing what I did. You don’t have to read it, I will also explain my code in text so that people who don’t have any coding experience can understand this too.")
 st.write("I started by downloading the datasets from Kaggle.com. I downloaded a zip-file in which I found 20 csv-files of cryptocurrencies. I loaded all the data of the CSV-files into pycharm and I made a whole dataset of all the cryptocurrencies. There were some uninteresting columns in the data so I had to delete those. I ended up with a dataset that contained the columns: “Name”, “Symbol”, “Price”, “Marketcap” and “Date” as the index. I only needed this data to start my project.")
 
-
-code = """
-    path = "/Users/mj/Desktop/Python For Datascience/Data_prices"
-    files = glob.glob(os.path.join(path + "/*.csv"))
-    data_frame = pd.DataFrame()
-    content = []
-    for filename in files:
-        df = pd.read_csv(filename, index_col=None)
-        content.append(df)
-        df["Name"] = filename
-    data_frame = pd.concat(content)
-    data_frame["Price"] = data_frame["Close"]
-    data_frame.drop(['Open', 'Low', 'High', 'SNo', 'Volume', 'Close'], inplace=True, axis=1)
-    data_frame = data_frame.reset_index()
-    split_column_name = data_frame["Name"].str.split("/", n=7, expand=True)
-    split_column_name = split_column_name[6]
-    split_column_name = split_column_name.str.split(".", n=1, expand=True)
-    split_column_name = split_column_name[0]
-    data_frame["Name"] = split_column_name
-    split_column_date = data_frame["Date"].str.split(" ", n=1, expand=True)
-    split_column_date = split_column_date[0]
-    data_frame["Date"] = split_column_date
-    data_frame = data_frame[data_frame['Date'] >= '2016-01-03']
-    data_frame = data_frame.reset_index()
-    data_frame.drop(['index', 'level_0'], inplace=True, axis=1)"""
-st.code(code, language="python")
 """After doing all this the dataset was looking like this:"""
 path_raw_data = "/Users/mj/Desktop/Python For Datascience/Raw Data.csv"
 df_raw = pd.read_csv(path_raw_data)
@@ -56,74 +30,6 @@ st.dataframe(df_raw)
 st.header("Step 2: Making My Own Index")
 st.write("To make my own index I needed to do a bit more work. I had to give all the cryptocurrencies a weight based on their market cap. So I started in 2016 and went all the way to July 2021 (this is where the data ends). But those weights change every time a new cryptocurrency enters the market. Not all the cryptocurrencies started around the same date. I started with 4 cryptos in 2016 and ended with 20 in 2021. Also every month I had to rebalance the index aswell, this meant again recalculating the weights of the cryptos.")
 st.write("Then making the index was not hard anymore. I just used the weight of the crypto and multiplied that with its price of that day and summed it up for all the cryptos. I divided this number by the sum of all the market caps of the cryptos in the index at that time and then you get a value for the FaMaLa index. All these values were stored in a new dataframe.")
-code_2 = """weights = []
-famala_index = 0
-famala_index_data = []
-date_list = []
-s = 0
-y = 0
-t = 0
-total_market_value = 0
-start_date = datetime.date(2016, 1, 3)
-rebalance_date = datetime.date(2016, 1, 3)
-end_date = datetime.date(2021, 7, 6)
-length_name_column_start = 4
-while start_date <= end_date:
-    start_date_string = start_date.strftime("%Y-%m-%d")
-    rebalance_date_string = rebalance_date.strftime("%Y-%m-%d")
-    date_df = data_frame[(data_frame['Date'] == start_date_string)]
-    length_name_column_date = len(date_df["Name"])
-    date_df = date_df.sort_values(["Marketcap"], ascending=False)
-    if length_name_column_start != length_name_column_date or rebalance_date_string == start_date_string:
-        while s < length_name_column_date:
-            total_market_value = total_market_value + date_df.iloc[s]['Marketcap']
-            s += 1
-        while y < length_name_column_date:
-            weight = (date_df.iloc[y]["Marketcap"] / total_market_value)
-            weights.append(weight)
-
-            weight = 0
-            y += 1
-        weights.sort(reverse=True)
-        while t < length_name_column_date:
-            famala_index = famala_index + (weights[t] * date_df.iloc[t]['Price'])
-            t += 1
-        famala_index_data.append(famala_index)
-        famala_index = 0
-        s = 0
-        t = 0
-        y = 0
-        total_market_value = 0
-        weights = []
-        length_name_column_start = length_name_column_date
-    else:
-        date_df = date_df.sort_values(["Marketcap"], ascending=False)
-        while s < length_name_column_start:
-            total_market_value = total_market_value + date_df.iloc[s]['Marketcap']
-            s += 1
-        while y < length_name_column_start:
-            weight = date_df.iloc[y]["Marketcap"] / total_market_value
-            weights.append(weight)
-
-            weight = 0
-            y += 1
-        weights.sort(reverse=True)
-        while t < length_name_column_start:
-            famala_index = famala_index + (weights[t] * date_df.iloc[t]['Price'])
-            t += 1
-        famala_index_data.append(famala_index)
-        famala_index = 0
-        s = 0
-        t = 0
-        y = 0
-        weights = []
-        total_market_value = 0
-    rebalance_date = rebalance_date + relativedelta(months=+3)
-    date_list.append(start_date_string)
-    start_date = start_date + datetime.timedelta(days=1)
-df_final = pd.DataFrame({"Date": date_list, "Famala Index": famala_index_data})
-df_final.set_index("Date", inplace=True)"""
-st.code(code_2, language="python")
 
 """That dataset looks like this: """
 st.dataframe(df_famala["Famala Index"])
@@ -137,34 +43,7 @@ st.write("In this part I looked at the returns of the index and compared them to
 st.write("After calculating all this I had to take out day 0 out of the data because on that day there is no return as I don’t have data before that day.")
 st.write("You can see the values of the index and the returns in this table.")
 st.dataframe(df_famala[["Famala Index", "Famala Index Return"]])
-code_4 = """cryptos = ["Aave", "Bitcoin", "Cardano", "ChainLink", "Cosmos", "Dogecoin", "CryptocomCoin", "XRP", "WrappedBitcoin", "Uniswap", "Tron",
-           "Stellar", "Solana", "Polkadot", "NEM", "EOS", "Ethereum", "Iota", "Litecoin", "Monero"]
 
-crypto_return = []
-for crypto in cryptos:
-    path_crypto = f"/Users/mj/Desktop/Python For Datascience/Data_prices/{crypto}.csv"
-    df_crypto = pd.read_csv(path_crypto)
-    split_column_date = df_crypto["Date"].str.split(" ", n=1, expand=True)
-    split_column_date = split_column_date[0]
-    df_crypto["Date"] = split_column_date
-    df_crypto = df_crypto[df_crypto['Date'] >= '2016-01-03']
-    df_crypto.set_index("Date", inplace=True)
-    df_crypto.drop(['Open', 'Low', 'High', 'SNo'], inplace=True, axis=1)
-    while q < len(df_crypto["Close"]):
-        return_day_1 = df_crypto.iloc[q]["Close"]
-        return_day_0 = df_crypto.iloc[q - 1]["Close"]
-        crypto_return.append(math.log(return_day_1/return_day_0))
-        q += 1
-    df_crypto = pd.DataFrame({f"{crypto} Return": crypto_return, f"{crypto} Price": df_crypto["Close"]})
-    df_crypto.drop(df_crypto.index[0], axis=0, inplace=True)
-    start_date_crypto = df_crypto.index[0]
-    df_crypto = pd.concat([df_crypto, df_final[df_final.index >= start_date_crypto]], axis=1)
-
-    df_crypto.to_csv(f"/Users/mj/Desktop/Python For Datascience/Crypto Returns/{crypto} Return.csv")
-    crypto_return = []
-    q = 0
-"""
-st.code(code_4, language="python")
 
 path_crypto = f"/Users/mj/Desktop/Python For Datascience/Crypto Returns/{selection} Return.csv"
 df_crypto = pd.read_csv(path_crypto)
@@ -190,13 +69,6 @@ f"""Intercept of the regression: {model.intercept_}.
 Percentual change of {selection} return when index changes by 1%: {model.coef_}%.
 Percentage of variation of {selection} explained by the model: {r_sq}% """
 
-code_3 = """crypto_array = df_crypto[f"{selection} Return"].to_numpy()
-famala_array = df_crypto["Famala Index Return"].to_numpy().reshape((-1,1))
-model = LinearRegression().fit(famala_array, crypto_array)
-r_sq = model.score(famala_array, crypto_array)
-fig = px.scatter(y=df_crypto[f"{selection} Return"], x=df_crypto["Famala Index Return"], trendline="ols", labels = {"x": "FaMaLa Index Returns","y": f"{selection} Returns"}, title="Scatterplot with lineair regression", trendline_color_override= "#FF0000")
-st.plotly_chart(fig, use_container_width=True)"""
-st.code(code_3, language="python")
 """
 As you can see because of the marketcap Bitcoin has, its correlation to the index is very big. You could almost say that the bitcoin on its own
 is a good indicator of the cryptomarket. It is even less volatile than the index itself. You can see how well the plot of the regression is when you compare Bitcoin to the FaMaLa index.
